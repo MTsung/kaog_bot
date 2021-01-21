0bb65cb01dc3f0a3a7c6d1b9e0a8e5f8c4e07d69
MTsung <money015037@gmail.com>
2021-01-21 21:13:22 +0800

    123

--- bot_start.sh
+++ bot_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ProcNumber=`ps -ef | grep 'kaog_bot.php' | grep -v 'grep' | wc -l`
 if [ $ProcNumber -eq 0 ];then
-    php /home/eykwwide/test_/kaog_bot.php &
+    php /home/eykwwide/public_html/cronjob/kaog_bot/kaog_bot.php &
 fi;
