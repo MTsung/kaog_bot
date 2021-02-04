78b1bda1d8a4407f40fa5d855af8ea3e9390eaec
MTsung <money015037@gmail.com>
2021-02-04 20:55:06 +0800

    !c哥 !c哥語錄總集篇

--- kaog_bot.php
+++ kaog_bot.php
@@ -57,6 +57,179 @@ $kago_text = [
 	],
 ];
 
+$c_text = [
+	[
+		'message' => '人品是一切之先行
+沒有人品 不用討論
+連話語權 出席權都沒有
+沒有意義',
+		'file' => null
+	],
+	[
+		'message' => '其實我不是沒良心，只是面對看不見盡頭的絕望與傷悲，
+
+我的心在一次次的受傷後，已經結出厚厚一層疤，
+
+然後我學會不輕易去碰觸它。我善於隱藏，用幽默嘲弄生命的無常；
+
+我試著不去看命運的殘酷，卻更感恩手裡能掌握的幸福。',
+		'file' => null
+	],
+	[
+		'message' => '朋友圈裡肯定有一個邏輯狂魔
+
+會瘋狂說別人邏輯有問題
+
+講一大堆別人根本不想看的東西
+
+好幾行 手都不會酸一樣
+
+就一直狂說自己立場的東西
+
+還會把自己帶入別人的角度來說
+
+邏輯狂魔到底在想什麼
+
+常常就是邏輯狂魔搞的氣氛超級尷尬
+
+你吵贏了這次 失去一個或多個朋友
+
+值得嗎 ? 邏輯狂魔',
+		'file' => null
+	],
+	[
+		'message' => '沒關係 一次多一點 貢獻國庫',
+		'file' => null
+	],
+	[
+		'message' => '沒關係 我一起送
+看他認不認為你在故意隱射',
+		'file' => null
+	],
+	[
+		'message' => '腦殘',
+		'file' => null
+	],
+	[
+		'message' => 'LLDX',
+		'file' => null
+	],
+	[
+		'message' => '邏輯死亡喔',
+		'file' => null
+	],
+	[
+		'message' => '單純邏輯問題',
+		'file' => null
+	],
+	[
+		'message' => '邏輯的問題',
+		'file' => null
+	],
+	[
+		'message' => '神邏輯',
+		'file' => null
+	],
+	[
+		'message' => '根本沒有邏輯',
+		'file' => null
+	],
+	[
+		'message' => '邏輯問題而已',
+		'file' => null
+	],
+	[
+		'message' => '這完全邏輯問題',
+		'file' => null
+	],
+	[
+		'message' => '重大聲明
+
+我從未也無意在網路上公開或PO出證明
+至少在巴哈姆特這種匿名論壇上沒有、也不可能會有
+來說明我是專業法律人這件事
+
+我沒有說我是不是專業法律人
+請不要預設立場 這也沒有意義
+你愛看就看 不愛看就滾
+你沒有權利及義務要求我證明什麼
+反之亦同 我沒有義務要拘束於你
+
+我有寫錯 友善提醒 非常歡迎
+我會做內容上合理的調整
+
+拜託一下 倫理規範不是塑膠 我會怕怕
+不要再跳針要我證明 一事不再理
+諭知免訴 不要一直拿過來重複說
+
+我只是一個 小小的
+星爆暨吉娃娃廢文專業巴友',
+		'file' => null
+	],
+	[
+		'message' => '不用懷疑我的專業  這沒有意義
+
+你只需要看我哪邊有寫錯
+
+跟我說一聲 我們一起把它寫好就好
+
+我歡迎合理且友善之討論 評判',
+		'file' => null
+	],
+	[
+		'message' => '不要問我幾歲 念哪裡畢業
+
+我不願意透露我的私人資訊
+
+再說
+
+你為何不懷疑我這隻帳號
+
+到底是不是多人使用一帳?',
+		'file' => null
+	],
+	[
+		'message' => '可憐
+辯不贏看我不爽罷了',
+		'file' => null
+	],
+	[
+		'message' => '踢??????',
+		'file' => null
+	],
+	[
+		'message' => '我沒有說錯任何法律相關阿',
+		'file' => null
+	],
+	[
+		'message' => '就沒有說錯東西啊',
+		'file' => null
+	],
+	[
+		'message' => '奇怪
+合法 為何我不能做',
+		'file' => null
+	],
+	[
+		'message' => '依法行言論阿',
+		'file' => null
+	],
+	[
+		'message' => '去我小屋首頁看',
+		'file' => null
+	],
+	[
+		'message' => '不用預設立場
+沒有意義
+我聲明過好幾次',
+		'file' => null
+	],
+	[
+		'message' => '我現在說 射在女童腿上 你們也不一定要看阿',
+		'file' => null
+	],
+];
+
 $sleep_text = [
 	[
 		'message' => '',
@@ -77,7 +250,7 @@ $client->on('event.MESSAGE_CREATE', function(DiscordClient $client, int $shard, 
 	    return;
 	}	
 
-    global $discord, $kago_text, $sleep_text, $console, $time, $count, $time_roll;
+    global $discord, $kago_text, $sleep_text, $c_text, $console, $time, $count, $time_roll;
     
     // sql 連線閒置太久會消失 要重 new 
     if(!$console->conn->Execute('SHOW TABLES;')){
@@ -94,6 +267,7 @@ $client->on('event.MESSAGE_CREATE', function(DiscordClient $client, int $shard, 
 	}
 	
 	$kaog = new MTsung\kaog($console,'bot_kaog','');
+	$cococola = new MTsung\dataList($console,'cococola','');
 
     $guild_id = $data['guild_id'];// 群組 id
     $user_id = $data['author']['id'];// user id
@@ -143,6 +317,7 @@ $client->on('event.MESSAGE_CREATE', function(DiscordClient $client, int $shard, 
 > !傑夫醬
 > !Arad_is_Jakads
 > !c哥
+> !c哥語錄總集篇
 > !特哥
 > !水沙蓮
 > !睡覺
@@ -186,10 +361,12 @@ $client->on('event.MESSAGE_CREATE', function(DiscordClient $client, int $shard, 
 	    	$discord->setMessage($channel_id, '', APP_PATH.'cronjob/kaog_bot/file/Arad.jpg');
 			break;
 		case '!c哥':
-	    	$discord->setMessage($channel_id, '人品是一切之先行
-沒有人品 不用討論
-連話語權 出席權都沒有
-沒有意義');
+	    	$key = rand(0, count($c_text) - 1);
+	        $discord->setMessage($channel_id, $c_text[$key]['message'], $c_text[$key]['file']);
+			break;
+		case '!c哥語錄總集篇':
+	    	$text = $cococola->getData('ORDER BY RAND() LIMIT 1');
+	        $discord->setMessage($channel_id, $text[0]['content']);
 			break;
 		case '!特哥':
 	    	$discord->setMessage($channel_id, '雷中之雷 貫中貫己 
