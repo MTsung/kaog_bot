111855e82575753d0b78d41f8a93ae2e9e8cf74b
MTsung <money015037@gmail.com>
2021-05-14 14:37:34 +0800

    Update kaog_bot.php

--- kaog_bot.php
+++ kaog_bot.php
@@ -239,6 +239,14 @@ $sleep_text = [
 		'message' => '',
 		'file' => APP_PATH.'cronjob/kaog_bot/file/www_sleep.png'
 	],
+	[
+		'message' => '',
+		'file' => APP_PATH.'cronjob/kaog_bot/file/arad_sleep.png'
+	],
+	[
+		'message' => '',
+		'file' => APP_PATH.'cronjob/kaog_bot/file/jeff_sleep1.jpeg'
+	],
 ];
 
 
