7e790eb893907aedc0e92e7a7d962f3c195e2d3a
MTsung <money015037@gmail.com>
2021-11-13 22:04:59 +0800

    敲擊幣滑倒榜

---
+++ command/aaaaaaa.php
@@ -0,0 +1,25 @@
+<?php
+
+class aaaaaaa implements command
+{
+    private $discord;
+    private $event;
+
+    public function __construct($event, $discord)
+    {
+        $this->discord = $discord;
+        $this->event = $event;
+    }
+
+    public function run()
+    {
+        $top = $this->event->discordUser()->getData('order by aaaaaaa desc limit 15');
+        $message = '敲擊幣滑倒榜
+';
+        foreach ($top as $key => $value) {
+            $message .= '> '.($key + 1).'. '.$value['member_nick'].' 滑倒了 '.$this->event->number_format_string($value['aaaaaaa']).' 次
+';
+        }
+        $this->discord->setMessage($this->event->channelId(), $message);
+    }
+}
