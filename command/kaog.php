72bfbe477e34beaa72bc51f381f9bdf4007b18a1
MTsung <money015037@gmail.com>
2021-11-13 22:05:23 +0800

    rename

--- command/kaog.php
+++ command/kaog.php
@@ -13,8 +13,8 @@ class kaog implements command
 
     public function run()
     {
-        $kago_text = include(APP_PATH.'cronjob/kaog_bot/text/kaog.php');
-        $key = rand(0, count($kago_text) - 1);
-        $this->discord->setMessage($this->event->channelId(), $kago_text[$key]['message'], $kago_text[$key]['file']);
+        $text = include(APP_PATH.'cronjob/kaog_bot/text/kaog.php');
+        $key = rand(0, count($text) - 1);
+        $this->discord->setMessage($this->event->channelId(), $text[$key]['message'], $text[$key]['file']);
     }
 }
