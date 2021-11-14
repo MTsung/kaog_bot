374de95295fdc4ebff8022046eb4e561e587c2be
MTsung <money015037@gmail.com>
2021-11-14 13:49:44 +0800

    config

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ $clientSecret = '';
 $token = '';
 
 
-define('MANIA_ID', '');
-define('ADMIN_ID', '');
-define('BET_CHANNEL_ID', '');
+define('KAOG_BOT_PATH', APP_PATH.'cronjob/kaog_bot/');
+define('MANIA_ID', '406747699841466371');
+define('ADMIN_ID', '327046840417517568');
+define('BET_CHANNEL_ID', '806901192654585867,406747700415954955,800260290779938826');
